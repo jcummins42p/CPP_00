614a909c308a32cf50f73942ff31e392d119b7e8
jcummins <jcummins42prague@gmail.com>
2024-08-29 15:45:57 +0200

    Added input options to help usability

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jcummins <jcummins@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 13:40:44 by jcummins          #+#    #+#             */
-/*   Updated: 2024/08/28 15:39:42 by jcummins         ###   ########.fr       */
+/*   Updated: 2024/08/29 15:44:46 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@ int		main(void)
 {
 	PhoneBook	phonebook;
 	std::string	command;
-	while (std::cout << "Enter a command: " && std::cin >> command)
+	while (std::cout << "Enter a command (EXIT (1) / ADD (2) / SEARCH (3)): "
+			&& std::cin >> command)
 	{
-		if (command == "EXIT")
+		if (command == "EXIT" || command == "1")
 			break;
-		else if (command == "ADD")
+		else if (command == "ADD" || command == "2")
 			phonebook.add();
-		else if (command == "SEARCH")
+		else if (command == "SEARCH" || command == "3")
 			phonebook.search();
 	}
 	return (0);
